410e12e35a0f41a39aec630a763643046b8561cc
Dan <bmdmartinez@gmail.com>
2021-11-16 17:17:22 -0500

    got dropdown to work for navbar

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -7,10 +7,16 @@ function Nav() {
                 Ginger Bennett
             </h1>
             <nav>
-            <ul className=" flex-row nav-list">
-                <li className="nav-list-item">About</li>
-                <li className="nav-list-item">Listings</li>
-            </ul>
+                <ul className=" flex-row nav-list">
+                    <div className="dropdown">
+                        <li className="nav-list-item">About</li>
+                        <div className="dropdown-item">
+                            <p>This Works</p>
+                            <a href="https://www.google.com" target="_blank" >Google</a>
+                        </div>
+                    </div>
+                    <li className="nav-list-item">Listings</li>
+                </ul>
             </nav>
         </header>
     )
