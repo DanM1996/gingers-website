e925345dc248b1d2662e7dfcf09c378b07dea121
Dan <bmdmartinez@gmail.com>
2021-12-20 11:15:24 -0500

    emailjs working for looking to buy, setting up looking to sell

--- src/components/Pages/lookingtobuy.js
+++ src/components/Pages/lookingtobuy.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { Button, Form } from 'semantic-ui-react'
-import './index.css'
-
+import { Button, Form } from 'semantic-ui-react';
+import emailjs from 'emailjs-com';
+import './index.css';
 
 function LookingToBuy() {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('sent');
+        emailjs.sendForm('service_r3nl70m', 'buy_contact', e.target, 'user_UoBjqfCNu4X3Hi0oti9xL')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text)
+        });
+        e.target.reset();
+    };
+ 
     return (
         <>
         <h1>Looking to Buy?</h1>
         <div>
             <p className='buyText'>If you're looking to buy a property in the Tampa area, look no further. We're here to help you find your dream home, all you need to do is fill out the form below and we'll be in touch soon.</p>
         </div>
-        <Form className="formSizing">
+        <Form className="formSizing" onSubmit={sendEmail}>
             <Form.Field>
                 <label>Full Name</label>
-                <input placeholder='Enter your full name' />
+                <input name="user_name" placeholder='Enter your full name' />
             </Form.Field>
             <Form.Field>
                 <label>Phone Number</label>
-                <input placeholder='Enter your phone number' />
+                <input name="user_phone" placeholder='Enter your phone number' />
             </Form.Field>
             <Form.Field>
                 <label>Email Address</label>
-                <input placeholder='Enter your email' />
+                <input name="user_email" placeholder='Enter your email' />
             </Form.Field>
-            <Button type='submit'>Submit</Button>
+            <Button type='submit' value="Send">Submit</Button>
         </Form>
         </>
     )
