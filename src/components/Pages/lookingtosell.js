eb52bc8409ebf17c6e79248a105012f386b24406
Dan <bmdmartinez@gmail.com>
2021-12-20 11:25:54 -0500

    emailjs works for both forms, will conduct futher testing to make sure it works repeatdely, styling of form pages to come

--- src/components/Pages/lookingtosell.js
+++ src/components/Pages/lookingtosell.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react'
+import emailjs from 'emailjs-com';
 import './index.css'
+
 function LookingToSell() {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('sent');
+        emailjs.sendForm('service_r3nl70m', 'sell_contact', e.target, 'user_UoBjqfCNu4X3Hi0oti9xL')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text)
+        });
+        e.target.reset();
+    };
+
     return (
         <>
         <h1>Looking to Sell?</h1>
         <div>
             <p className='sellText'>If you're looking to sell your home and need a realtor, we're happy to help you. All you need to do is fill out the form below with the required information and we'll be in touch with you.</p>
         </div>
-        <Form className="formSizing">
+        <Form className="formSizing" onSubmit={sendEmail}>
             <Form.Field>
                 <label>Full Name</label>
-                <input placeholder='Enter your full name' />
+                <input name="user_name" placeholder='Enter your full name' />
             </Form.Field>
             <Form.Field>
                 <label>Phone Number</label>
-                <input placeholder='Enter your phone number' />
+                <input name="user_phone" placeholder='Enter your phone number' />
             </Form.Field>
             <Form.Field>
                 <label>Email Address</label>
-                <input placeholder='Enter your email' />
+                <input name="user_email" placeholder='Enter your email' />
             </Form.Field>
             <Button type='submit'>Submit</Button>
         </Form>
